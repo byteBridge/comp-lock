fe0fa50c50b196ef322bca2652e9cc2c86588320
Kudakwashe Paradzayi <kgparadzayi@gmail.com>
2017-07-25 12:59:34 +0200

    Remove the splash screen

--- CompLock/My Project/Application.Designer.vb
+++ CompLock/My Project/Application.Designer.vb
@@ -34,10 +34,5 @@ Namespace My
         Protected Overrides Sub OnCreateMainForm()
             Me.MainForm = Global.CompLock.frmLogin
         End Sub
-        
-        <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
-        Protected Overrides Sub OnCreateSplashScreen()
-            Me.SplashScreen = Global.CompLock.frmViewAccounts
-        End Sub
     End Class
 End Namespace
