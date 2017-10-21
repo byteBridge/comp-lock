c6c409adb9d9bfcfd0e015459c0a44c7c07f0f8e
Kudakwashe Paradzayi <kgparadzayi@gmail.com>
2017-10-21 18:20:05 +0200

    Major ui cleanup, remove all unused elements and point admin to the web ui

--- CompLock/Administrators/frmAdminMainMenu.vb
+++ CompLock/Administrators/frmAdminMainMenu.vb
@@ -5,30 +5,30 @@
     ''' </summary>
     Property NameOfAdministrator As String
 
-    Private Sub btnSignOut_Click(sender As Object, e As EventArgs) Handles btnSignOut.Click
+    Private Sub btnSignOut_Click(sender As Object, e As EventArgs)
         Me.Close()
     End Sub
 
-    Private Sub btnCreateAccount_Click(sender As Object, e As EventArgs) Handles btnCreateAccount.Click
+    Private Sub btnCreateAccount_Click(sender As Object, e As EventArgs)
         frmCreateAccount.ShowDialog()
     End Sub
 
-    Private Sub btnViewAccounts_Click(sender As Object, e As EventArgs) Handles btnViewAccounts.Click
+    Private Sub btnViewAccounts_Click(sender As Object, e As EventArgs)
         frmViewAccounts.NameOfAdministrator = NameOfAdministrator
         frmViewAccounts.ShowDialog()
 
     End Sub
 
-    Private Sub btnReports_Click(sender As Object, e As EventArgs) Handles btnReports.Click
+    Private Sub btnReports_Click(sender As Object, e As EventArgs)
         FrmReports.NameOfAdministrator = NameOfAdministrator
         FrmReports.ShowDialog()
     End Sub
 
-    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+    Private Sub Button3_Click(sender As Object, e As EventArgs)
         frmSettings.ShowDialog()
     End Sub
 
-    Private Sub btnShutDown_Click(sender As Object, e As EventArgs) Handles btnShutDown.Click
+    Private Sub btnShutDown_Click(sender As Object, e As EventArgs)
         If MessageBox.Show("Are you sure you want to quit this program?", "Really sure?", MessageBoxButtons.YesNo, MessageBoxIcon.Question) = Windows.Forms.DialogResult.Yes Then
             Application.Exit()
         End If
@@ -39,7 +39,7 @@
         lblName.Text = "Hello, " & NameOfAdministrator
     End Sub
 
-    Private Sub btnMinimise_Click(sender As Object, e As EventArgs) Handles btnMinimise.Click
+    Private Sub btnMinimise_Click(sender As Object, e As EventArgs)
         Me.WindowState = FormWindowState.Minimized
     End Sub
 
@@ -49,4 +49,13 @@
 
         End If
     End Sub
+
+    Private Sub btnOpenAdminConsole_Click(sender As Object, e As EventArgs) Handles btnOpenAdminConsole.Click
+        Try
+            Process.Start("http://localhost:3000/#/admin")
+        Catch ex As Exception
+
+        End Try
+    End Sub
+
 End Class
